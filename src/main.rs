88efb908c284ade375cc33f55c7ef78e4939be8e
Jeron Aldaron Lau <aldaronlau@gmail.com>
2024-10-24 22:01:32 -0500

    Match all segments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use axum::{
-    routing::get,
+    routing,
     Router,
+    extract::Path,
 };
+
+async fn handler(Path(path): Path<String>) -> String {
+    path
+}
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
+    let app = Router::new().route("/{*path}", routing::get(handler));
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
     axum::serve(listener, app).await.unwrap();
 }
